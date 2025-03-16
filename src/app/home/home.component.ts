d5795fa88e987067ea2a49020a9b6d30a86678f4
jovana722 <jovana.milicevic.22@singimail.rs>
2025-03-16 20:14:13 +0100

    Connected to movie api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,13 @@ import { AxiosError } from 'axios';
 import { MovieService } from '../../services/movie.service';
 import { NgFor, NgIf } from '@angular/common';
 import { MovieModel } from '../../models/movie.model';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { UtilsService } from '../../services/utils.service';
 
 @Component({
   selector: 'app-home',
-  imports: [ NgIf, NgFor, MatButtonModule, MatCardModule],
+  imports: [NgIf, NgFor, MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -17,11 +17,12 @@ export class HomeComponent {
   public movies: MovieModel[] | null = null
   public error: string | null = null
 
-  constructor(public utils: UtilsService){
-    MovieService.getMovies(0, 3)
-      .then(rsp => this.movies = rsp.data.content )
+  constructor(public utils: UtilsService) {
+    MovieService.getMovies()
+      .then((rsp) => this.movies = rsp.data
+      )
       .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`);
   }
-    
-  
+
+
 }
